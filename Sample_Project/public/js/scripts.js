ddb1ac487d561a602b475b9aa965d9dc5e8aefdd
Alston <and_croning@hotmail.com>
2015-11-26 02:42:43 -0500

    SampleProject files are preparing for Phalcon framwork with a new project

---
+++ Sample_Project/public/js/scripts.js
@@ -0,0 +1,15 @@
+//document objects
+var msg = document.getElementById("message");
+var button = document.getElementById("button");
+
+//Hello world message
+msg.innerHTML += "<p>Hello World!!</p>";
+
+//click event for the button
+button.addEventListener("click",ChangeColor,false);
+
+
+//Changes Colors
+function ChangeColor() {
+    msg.style.color = "blue";
+}
