43cf0ba6fe57f1e5858cca68586014cfb967cbfe
Alston <and_croning@hotmail.com>
2015-11-28 00:22:52 -0500

    Preparing a Signup controller for Sample Project

---
+++ Sample_Project/app/controllers/SignupController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dishan Croning
+ * Date: 11/28/2015
+ * Time: 12:19 AM
+ */
+
+use Phalcon\mvc\Controller;
+
+class SignupController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+}
+?>
