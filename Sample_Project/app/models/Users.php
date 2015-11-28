5300ef0b44a8ae293a304611690dee359dcabc05
Alston <and_croning@hotmail.com>
2015-11-28 02:01:16 -0500

    Creating a model for the Sample Project

---
+++ Sample_Project/app/models/Users.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dishan Croning
+ * Date: 11/28/2015
+ * Time: 1:57 AM
+ */
+
+use Phalcon\Mvc\Model;
+
+class Users extends Model
+{
+    public $id;
+
+    public $name;
+
+    public $email;
+}
+
+?>
