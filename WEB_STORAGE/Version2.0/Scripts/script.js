14d845b73c6365b180b23f7447dce1eea9823302
Alston <and_croning@hotmail.com>
2016-01-19 02:22:14 -0500

    Validated data, displays data clearly

--- WEB_STORAGE/Version2.0/Scripts/script.js
+++ WEB_STORAGE/Version2.0/Scripts/script.js
@@ -8,16 +8,16 @@ var SS = window.sessionStorage;
 var LS = window.localStorage;
 var str1 = "";
 var str2 = "";
+var input1, input2;
 
-//Create Click Event
-SaveButton.addEventListener("click",AccessData,true);
+//Declare Events
+SaveButton.addEventListener("click",Store_Data,false);
 ClearButton.addEventListener("click",ClearData,false);
 
-
-function AccessData() {
-    SS.setItem("a",Access_Data1.value);
-    SS.setItem("b",Access_Data2.value);
-    Store_Data();
+function Store_Data() {
+    SS.setItem("a", Access_Data1.value);
+    SS.setItem("b", Access_Data2.value);
+    Read_AND_DisplayData();
 }
 
 function ClearData(){
@@ -27,27 +27,37 @@ function ClearData(){
     Access_Data2.value = "";
 }
 
-function Store_Data(){
-    var k, count = 0;
-    for( k in SS ){
-        var Temp_SS = SS.getItem(k)
+function Read_AND_DisplayData(){
 
-        if(Temp_SS != null) {
+    //variables
+    var k, count = 0;
+    input1 = Access_Data1.value;
+    input2 = Access_Data2.value;
+
+    //Read Data
+    for( k in SS ){
+        var Temp_SS = SS.getItem(k);
+
+        if (Temp_SS != null) {
             count++;
-            if(count == 1) {
-                str1 = SS.getItem(k) + "/";
+            if (count == 1) {
+                str1 = SS.getItem(k) + "<br/><br/>";
             }
-            if(count == 2){
-                str2 = SS.getItem(k);
+            if (count == 2) {
+                str2 = SS.getItem(k) + "<br/>";
                 count = 0;
             }
         }
     }
-    display();
-}
 
-function display(){
-    if(str1.concat(str2) != "/" ) {
-        msg.innerHTML += str1.concat(str2)+ "<br/>";
+    //Display Data
+    if( (input1 != "") && (input2 != "") ){
+        msg.innerHTML += "Input1: " + str2.concat("Input2: " + str1);
+    }
+    else if(input1 != ""){
+        msg.innerHTML += "Input1: " + str2.concat(str1);
+    }
+    else if((input2 != "")){
+        msg.innerHTML += str2.concat("Input2: " + str1);
     }
 }
