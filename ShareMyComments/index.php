f1d39cb11cb6ca6ed48f5148f3d89146738845e5
Alston <and_croning@hotmail.com>
2016-03-23 04:33:53 -0400

    some success with database interactivity, indent feature, and design. to be continued..

--- ShareMyComments/index.php
+++ ShareMyComments/index.php
@@ -10,16 +10,26 @@
 
 <body>
 
+<?php
+$link = mysqli_connect("localhost","root","a1xcr0nge","sampledatabase");
+$rows = "";
+if(mysqli_connect_error()){
+    die("couldn't connect to Database");
+}
+?>
+
+
 <header>Share Your Comments Here</header>
 
 
 <div id="wrapper">
-<form method="post" action="index.php">
+<form method="post" action="<?php $_SERVER['PHP_SELF'];?>">
 
     Comment Here<br />
-    <textarea id="inputComment" name="inputComment1" cols="30" rows="2" autofocus ></textarea><br />
-    <textarea id="displayComment" name="displayComment1" cols="30" rows="2" disabled>Output</textarea><br />
-    <button id="submitButton">Post Comment</button>
+    <textarea id="inputComment" name="inputComment1" cols="30" rows="2" autofocus ></textarea>
+    <input type="submit" id="submitButton" /><br />
+
+    <textarea id="displayComment" name="displayComment1" cols="50" rows="10" disabled ></textarea><br />
 
 </form>
 </div>
@@ -27,6 +37,23 @@
 <!--
 <footer>Copyright &copy All Rights reserved for Alston Croning</footer>
 -->
+
+<?php
+$comment = $_POST['inputComment1'];
+if($comment != ""){
+    $query = "INSERT INTO `users` (`name`,`email`) VALUES ('hello','$comment')";
+    mysqli_query($link, $query);
+    /*
+            if($result = mysqli_query($link,$query)) {
+                while($rows = mysqli_fetch_array($result)) {
+                    //print_r($rows);
+                }
+            }*/
+
+//print $comment;
+}
+?>
+
 
     <script src="JavaScript.js"></script>
 </body>
