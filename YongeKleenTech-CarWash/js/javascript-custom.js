c2f83521d7a9cf3ac86d553af35e5c85fe9286ec
Alston <and_croning@hotmail.com>
2016-09-15 21:19:35 -0400

    Home, About Us, Services, and Gallery pages added for YongeKleenTech Car Wash Website

---
+++ YongeKleenTech-CarWash/js/javascript-custom.js
@@ -0,0 +1,50 @@
+//exterior
+$(document).ready(function () {
+    $('.bxslider-exterior').bxSlider({
+        slideWidth: 400,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 3,
+        response: true,
+        slideMargin: 5,
+        pause: 4000,
+        autoHover: true,
+        controls: true,
+        captions: true,
+        touchEnabled: true
+    });
+});
+
+//interior
+$(document).ready(function () {
+    $('.bxslider-interior').bxSlider({
+        slideWidth: 400,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 3,
+        response: true,
+        slideMargin: 5,
+        pause: 5000,
+        autoHover: true,
+        controls: true,
+        captions: true,
+        touchEnabled: true
+    });
+});
+
+//other
+$(document).ready(function () {
+    $('.bxslider-other').bxSlider({
+        slideWidth: 400,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 3,
+        response: true,
+        slideMargin: 5,
+        pause: 6000,
+        autoHover: true,
+        controls: true,
+        captions: true,
+        touchEnabled: true
+    });
+});
